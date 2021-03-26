a277e4aaff4a41bff03652f15b2a24225ce549be
mrcheater <mister.cheater@list.ru>
2021-03-26 21:31:28 +0300

    WIP

--- src/server/index.ts
+++ src/server/index.ts
@@ -18,45 +18,60 @@ export type ValuesOfTuple<
 > = T extends ReadonlyArray<any> ? T[number] : never;
 
 class Domain<
+  DatabaseName extends string,
+  EventStoreCollectionName extends string,
   Events extends Record<string, t.TypeC<any>>,
-  EventTypes extends ReadonlyArray<Narrowable> = [],
   Projections extends Record<
     string,
     {
       name: Narrowable;
       handlers: Partial<Record<ValuesOfTuple<EventTypes>, boolean>>;
     }
-  > = {}
+  > = {},
+  EventTypes extends ReadonlyArray<Narrowable> = []
 > {
-  readonly events: Events = {} as any;
-  readonly eventTypes: EventTypes = [] as any;
-  readonly projections: Projections = {} as any;
+  readonly databaseName: DatabaseName;
+  readonly eventStoreCollectionName: EventStoreCollectionName;
+  readonly events: Events;
+  readonly eventTypes: EventTypes;
+  readonly projections: Projections;
   constructor(
+    databaseName: DatabaseName = null,
+    eventStoreCollectionName: EventStoreCollectionName = null,
     events: Events = [] as any,
     eventTypes: EventTypes = [] as any,
     projections: Projections = {} as any
   ) {
+    this.databaseName = databaseName;
+    this.eventStoreCollectionName = eventStoreCollectionName;
     this.events = events;
     this.eventTypes = eventTypes;
     this.projections = projections;
   }
   eventType<EventType extends string>(
     eventType: EventType
-  ): Domain<Events, readonly [...EventTypes, EventType], Projections> {
-    return new Domain<any, any, any>(
+  ): Domain<
+    DatabaseName,
+    EventStoreCollectionName,
+    Events,
+    Projections,
+    readonly [...EventTypes, EventType]
+  > {
+    return new Domain<any, any, any, any, any>(
+      this.databaseName,
+      this.eventStoreCollectionName,
       { ...this.events },
-      [...this.eventTypes, eventType],
-      {
-        ...this.projections,
-      }
+      { ...this.projections },
+      [...this.eventTypes, eventType]
     );
   }
   projection<Name extends string>(projection: {
     name: Name;
     handlers: Partial<Record<ValuesOfTuple<EventTypes>, boolean>>;
   }): Domain<
+    DatabaseName,
+    EventStoreCollectionName,
     Events,
-    readonly [...EventTypes],
     Projections &
       Record<
         Name,
@@ -64,28 +79,79 @@ class Domain<
           name: Name;
           handlers: Partial<Record<ValuesOfTuple<EventTypes>, boolean>>;
         }
-      >
+      >,
+    readonly [...EventTypes]
   > {
-    return new Domain<any, any, any>({ ...this.events }, [...this.eventTypes], {
-      ...this.projections,
-      [projection.name]: projection,
-    });
+    return new Domain<any, any, any, any, any>(
+      this.databaseName,
+      this.eventStoreCollectionName,
+      { ...this.events },
+      {
+        ...this.projections,
+        [projection.name]: projection,
+      },
+      [...this.eventTypes]
+    );
   }
 
   event<Type extends string, PayloadSchema extends Record<string, any>>(
     type: Type,
     payloadSchema: PayloadSchema
   ): Domain<
+    DatabaseName,
+    EventStoreCollectionName,
     Events & Record<Type, t.TypeC<PayloadSchema>>,
-    readonly [...EventTypes, Type],
-    Projections
+    Projections,
+    readonly [...EventTypes, Type]
   > {
-    return new Domain<any, any, any>(
+    return new Domain<any, any, any, any, any>(
+      this.databaseName,
+      this.eventStoreCollectionName,
       { ...this.events, [type]: t.type(payloadSchema) },
-      [...this.eventTypes, type],
       {
         ...this.projections,
-      }
+      },
+      [...this.eventTypes, type]
+    );
+  }
+
+  database<DatabaseName extends string>(
+    name: DatabaseName
+  ): Domain<
+    DatabaseName,
+    EventStoreCollectionName,
+    Events,
+    Projections,
+    readonly [...EventTypes]
+  > {
+    return new Domain<any, any, any, any, any>(
+      name,
+      this.eventStoreCollectionName,
+      { ...this.events },
+      {
+        ...this.projections,
+      },
+      [...this.eventTypes]
+    );
+  }
+
+  eventStore<EventStoreCollectionName extends string>(
+    name: EventStoreCollectionName
+  ): Domain<
+    DatabaseName,
+    EventStoreCollectionName,
+    Events,
+    Projections,
+    readonly [...EventTypes]
+  > {
+    return new Domain<any, any, any, any, any>(
+      this.databaseName,
+      name,
+      { ...this.events },
+      {
+        ...this.projections,
+      },
+      [...this.eventTypes]
     );
   }
 }
@@ -93,6 +159,8 @@ class Domain<
 const domain = () => new Domain();
 
 const app = domain()
+  .database("dbname")
+  .eventStore("events")
   .event("create", { a: t.boolean })
   .event("drop", { b: t.number })
   .event("dispose", { c: t.string })
@@ -109,6 +177,9 @@ const app = domain()
       dispose: true,
     },
   });
+
+const dbname = app.databaseName;
+const events = app.eventStoreCollectionName;
 
 type createT = t.TypeOf<typeof app.events.create>;
 type dropT = t.TypeOf<typeof app.events.drop>;
